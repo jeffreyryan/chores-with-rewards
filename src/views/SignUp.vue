575213e1e5f324e4c3f23147bfa2dd841b8e220e
EC2 Default User <ec2-user@ip-172-31-41-220.ec2.internal>
2019-09-05 03:56:34 +0000

    with signin and signup

---
+++ src/views/SignUp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <SignUp />
+  </div>
+</template>
+
+<script>
+import SignUp from '../components/signup';
+
+export default {
+  components: {
+    SignUp 
+  },
+};
+</script>
