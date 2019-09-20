37f7cf8db00cc7ed2f5344b2d12d4c2fdc4fae1d
EC2 Default User <ec2-user@ip-172-31-41-220.ec2.internal>
2019-09-20 14:06:25 +0000

    added padding to content in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,7 @@
 <template>
   <v-app>
     <Navbar />
-    <!-- <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar> -->
-
-    <v-content>
+    <v-content class="mx-4 mb-4">
       <!-- <HelloWorld/> -->
       <router-view></router-view>
     </v-content>
