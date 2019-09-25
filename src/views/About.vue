b77684e9d8671dc398f581bcf51b2e681bec529f
EC2 Default User <ec2-user@ip-172-31-41-220.ec2.internal>
2019-09-25 15:16:05 +0000

    Improved About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="about">
     <h1 class="subheading grey--text">About the Creators</h1>
-    <p class="red white--text">On July 20th 2019, Jayden Ryan had an idea for an app which she told her dad.  Her Dad (Jeff) asked her to draw(plan) her idea so they could work together on it.</h1>
+    <p>On July 20th 2019, Jayden Ryan had an idea for an app which she told her dad.  Her Dad (Jeff) asked her to draw(plan) her idea so they could work together on it.</h1>
     <br>
-    <h1> While building this app we wanted to give children like Jayden the ability to do the following:</h1>
-          <ul>
-            <li>1. Come up with a idea. (Reward)</li>
-            <li>2. Plan how to accomplish the idea. (Chores)</li>
-            <li>3. Get others to help.</li>
-            <li>4. Finish the chores and enjoy the rewards.</li>
-          </ul>
+    <p> While building this app we wanted to give children like Jayden the ability to do the following:</h1>
+          <ol>
+            <li> Come up with a idea. (Reward)</li>
+            <li> Plan how to accomplish the idea. (Chores)</li>
+            <li> Get others to help.</li>
+            <li> Finish the chores and enjoy the rewards.</li>
+          </ol>
     </p>
     <br>
-    <p class="pink lighten-4 red--text text--darken-4">With that in mind, we created an app for kids.  The app can be used to track rewards provided by parents and how the reward can be earned. It can also be used to track child's can set their own rewards and get them. 
+    <p>With that in mind, we created an app for kids.  The app can be used to track rewards provided by parents and how the reward can be earned. It can also be used to track child's can set their own rewards and get them. 
     </h1>
     <h1 class="subheading grey--text">Examples</h1>
   </div>
