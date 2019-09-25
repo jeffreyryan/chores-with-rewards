5a2bb1ebc2d306ae784c2966111b3dc6b0ff6165
EC2 Default User <ec2-user@ip-172-31-41-220.ec2.internal>
2019-09-25 14:20:00 +0000

    Added Vision to About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>On July 20th 2019, Jayden Ryan had an idea for an app which she told her dad.  Her Dad (Jeff) asked her to draw what she had planned.</h1>
+    <br>
+    <h1> While building this app I wanted to give children like my daughter the ability to do the following:
+            1. Come up with a idea.
+            2. Plan how to accomplish the idea.
+            3. Enlist other's support.
+            4. Execute the plan and enjoy the rewards.
+    </h1>
+    <br>
+    <h1>With that in mind, we created an app for kids.  The app can be used to track Rewards provided by parents and how the reward can be earned. It can also be used to track child's can set their own rewards and get them.  How to:
+            1. Come up with the idea. Create a Reward
+            2. Plan how to accomplish the idea. List out the Chores needed to get the Reward.
+            3. Sometimes others posess the reward.  They must agree to give the reward.  Create the Chores needed
+            4. Check off the tasks until they get their reward.
+    </h1>
   </div>
 </template>
