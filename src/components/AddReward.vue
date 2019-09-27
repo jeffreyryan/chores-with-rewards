f2b04d264ddc3437342d6b2feb933362cb1ed73a
EC2 Default User <ec2-user@ip-172-31-41-220.ec2.internal>
2019-09-27 15:53:16 +0000

    adding rewards frontend

---
+++ src/components/AddReward.vue
@@ -0,0 +1,78 @@
+<template>
+    <v-dialog max-width="600px" v-model="dialog">
+        <template v-slot:activator="{ on }">
+            <v-btn text class="success" v-on="on" >Add new reward</v-btn>
+        </template>
+        <v-card>
+            <v-card-title>
+                <h2>Add a New Reward</h2>
+            </v-card-title>
+            <v-card-text>
+                <v-form class="px-3" ref="rewardForm">
+                  <v-text-field label="Title" v-model="title" prepend-icon="folder" :rules="inputRules"></v-text-field>
+                  <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+                  
+                  <v-menu
+                      v-model="menu1"
+                      :close-on-content-click="false"
+                      full-width
+                      max-width="290"
+                  >
+                      <template v-slot:activator="{ on }">
+                          <v-text-field
+                              :value="due"
+                              clearable
+                              label="Due Date"
+                              readonly
+                              v-on="on"
+                          ></v-text-field>
+                      </template>
+                      <v-date-picker
+                          v-model="due"
+                          @change="menu1 = false"
+                      ></v-date-picker>
+                  </v-menu>
+                  <v-spacer></v-spacer>
+
+                  <v-btn flat class="success mx-0 mt-3" :loading="addingReward" @click="submit">Add Reward</v-btn>
+                </v-form>
+            </v-card-text>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+    export default{
+      data() {
+        return {
+          title: '',
+          content: '',
+          due: null,
+          addingReward: false,
+          dialog: false,
+          inputRules: [
+             v => v.length >= 1 || 'This field is required'
+          ]
+        }
+      },
+      methods: {
+        submit() {
+           if(this.$refs.rewardForm.validate()) {
+              this.addingReward=true;
+              setTimeout(function(){
+                console.log(this.title , this.content);
+              },1)
+            console.log(this.content);
+           }
+           this.addingReward=false;
+           this.dialog=false;
+           // this.$emit('choreAdded');
+        }
+      },
+       //computed: {
+        //  formattedDate(){
+         //    return this.due ? format(this.due,'Do MMM YYYY') : '' 
+          //}
+       //}
+    }
+</script>
---
+++ src/views/Rewards.vue
@@ -0,0 +1,48 @@
+<template>
+   <div class="Rewards">
+       <h1>Rewards</h1>
+       <v-container class="my-5">
+           <v-expansion-panels>
+               <v-expansion-panel
+                   v-for="cwr in myRewards"
+                   :key="reward"
+               >
+                   <v-expansion-panel-header>{{ cwr.chore }}</v-expansion-panel-header>
+                   <v-expansion-panel-content>
+                       <v-card>
+                          <v-card-text class="px-4 grey--text">
+                             <div class="font-weight-bold">due by {{ cwr.nextDueDate }}</div>
+                             <div>info</div>
+                          </v-card-text>
+                       </v-card>
+                   </v-expansion-panel-content>
+               </v-expansion-panel>
+           </v-expansion-panels>
+       </v-container>
+       <AddChore />
+   </div>
+</template>
+
+<script>
+import AddReward from '../components/AddReward';
+
+export default {
+  components: { AddReward },
+  data(){
+    return {
+      choresWithRewards: [
+         {reward: '$5', person:'jayden', reward: '$5', nextDueDate: '08/24/2019', status: 'Complete'},
+         {reward: '$5', person:'jay',  reward: 'Icecream', nextDueDate: '08/26/2019', status: 'RewardPending'},
+         {reward: 'Dad Dancing', person:'jayden', reward: '$2', nextDueDate: '08/30/2019', status: 'ChorePending'}
+      ]
+    }
+  },
+   computed: {
+      myRewards() {
+         return this.choresWithRewards.filter(chore => {
+            return chore.person === 'jayden'
+         })
+      }
+   }
+};
+</script>
