f3100f827fc5bb836d2ee24e7f65197af2d0ab13
EC2 Default User <ec2-user@ip-172-31-41-220.ec2.internal>
2019-11-10 14:26:01 +0000

    changed router mode to hash

--- src/router.js
+++ src/router.js
@@ -6,7 +6,8 @@ import SignUp from "./views/SignUp.vue";
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
+  // mode: "history",
+  mode: "hash",
   base: process.env.BASE_URL,
   routes: [
     {
