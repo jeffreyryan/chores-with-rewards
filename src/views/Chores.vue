5687b8f4f292155c54b5c29de9f96c17a5e0e6da
EC2 Default User <ec2-user@ip-172-31-41-220.ec2.internal>
2019-12-08 14:35:30 +0000

    displaying db chores

--- src/views/Chores.vue
+++ src/views/Chores.vue
@@ -4,52 +4,26 @@
        <v-container class="my-5">
            <v-expansion-panels>
                <v-expansion-panel
-                   v-for="cwr in myChores"
-                   :key="cwr.chore"
-               >
-                   <v-expansion-panel-header>{{ cwr.chore }}</v-expansion-panel-header>
-                   <v-expansion-panel-content>
-                       <v-card>
-                          <v-card-text class="px-4 grey--text">
-                             <div class="font-weight-bold">due by {{ cwr.nextDueDate }}</div>
-                             <div>info</div>
-                          </v-card-text>
-                       </v-card>
-                   </v-expansion-panel-content>
-               </v-expansion-panel>
-               <v-expansion-panel
                   v-for="cwr in myDbChores.items"
                     :key="cwr.title"
                  @click="panelExpanded(cwr.reward.name)"
                >
                   <v-expansion-panel-header>{{ cwr.title }}</v-expansion-panel-header>
-                  <v-expansion-panel-content>
+                  <v-expansion-panel-content @click="panelExpanded(cwr.reward.name)" >
                       <v-card>
                           <v-card-text class="px-4 grey--text">
                               <div class="font-weight-bold">due by NEED DATES</div>
                               <div>info</div>
-                              <div class="font-weight-bold">{{ cwr.reward && cwr.reward.name }}</div>
-                              <v-btn class="success" @click="consoleReward">Console Reward</v-btn>
-                              <v-select
-                                    :items="rewards"
-                                    v-model="setLocale"
-                                    label="Reward"
-                                    item-value="name"
-                                    @change='updateReward'
-                              ></v-select>
+                              <div class="font-weight-bold">Reward : {{ cwr.reward && cwr.reward.name }}</div>
+                              <!-- <UpdateChore :choreID="cwr.id"/> -->
                           </v-card-text>
                       </v-card>
+                      <v-container class="d-flex flex-row-reverse"> 
+                          <UpdateChore :choreID="cwr.id"/>
+                      </v-container>
                    </v-expansion-panel-content>
                </v-expansion-panel> 
            </v-expansion-panels>
-           <v-select
-               :items="dbRewards"
-               v-model="setLocale"
-               label="Reward"
-               item-value="name"
-               item-text="id"
-               @change='updateReward'
-           ></v-select>
        </v-container>
        <AddChore />
    </div>
@@ -57,40 +31,31 @@
 
 <script>
 import AddChore from '../components/AddChore';
-import * as mutations from "../graphql/mutations";
-import { API, graphqlOperation } from "aws-amplify";
+import UpdateChore from '../components/UpdateChore';
 
 export default {
-  components: { AddChore },
+  components: { AddChore,UpdateChore },
   data(){
     return {
-      choresWithRewards: [
-         {chore: 'Take out the trash', person:'jayden', reward: '$5', nextDueDate: '08/24/2019', status: 'Complete'},
-         {chore: 'Clean Room', person:'jay',  reward: 'Icecream', nextDueDate: '08/26/2019', status: 'RewardPending'},
-         {chore: 'Give Dogs Bath', person:'jayden', reward: '$2', nextDueDate: '08/30/2019', status: 'ChorePending'}
-      ],
-      rewards: ['$5','$1','DQ Icecream'],
-      dbRewards: [{id:'1',name:'$1'},{id:'2',name:'$5'}],
-      initValue:'1',
-      fetchedLocale: '',
       setLocale: null
     }
   },
    created(){
-     setTimeout(() => {
-       this.fetchedLocale = '$1';
-       this.setLocale = this.fetchedLocale;
-     }, 1000);
+     this.$store.dispatch('populatedbUser','jryan');
+     this.$store.dispatch('populateRewards','jryan');
    },
    computed: {
-      myChores() {
-         return this.choresWithRewards.filter(chore => {
-            return chore.person === 'jayden'
-         })
-      },
+      //myChores() {
+      //   return this.choresWithRewards.filter(chore => {
+      //      return chore.person === 'jayden'
+      //   })
+      //},
       myDbChores(){
-          this.$store.dispatch('populatedbUser','jryan');
+          console.log('computing myDbChores');
           return this.$store.getters.choresWithRewards;
+      },
+      myDbRewards(){
+          return this.$store.getters.rewards;
       },
       dbUser(){
           //return 'jryan';
@@ -102,36 +67,7 @@ export default {
       { 
          console.log('Inside panelExpanded');
          console.log(value);
-         console.log(this.setLocale);
-         setTimeout(() => {
-            this.setLocale='$5';
-         }, 1000);
-         console.log(this.setLocale);
       },
-      updateReward(value)
-      {
-         console.log('update choresWithRewards');
-         const choreLinkage = {
-             id:'5393245d-35ff-4ed0-a717-4f9050cbb1a0',
-              choreRewardId:'3341286d-5f6e-4543-af95-8eaf8038da00'
-             }
-         const updateChore = API.graphql(
-             graphqlOperation(mutations.updateChore, {input: choreLinkage })
-         )
-              .then (res => {
-                 console.log('update worked');
-               })
-              .catch(err => {
-                 console.log(err.message);
-               })
-         
-      },
-      consoleReward()
-          {
-           console.log('In consoleReward');
-           console.log(this.myDbChores.items[1].title);
-           console.log(this.myDbChores.items[1].reward.name);
-          },
    },
 };
 </script>
