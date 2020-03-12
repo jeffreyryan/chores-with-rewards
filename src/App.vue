193fa102ed709e8a305781a3d34af0a9524d7c12
EC2 Default User <ec2-user@ip-172-31-41-220.ec2.internal>
2020-03-12 11:18:33 +0000

    footer

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,24 @@
   <v-app>
     <Navbar />
     <v-content class="mx-4 mb-4">
-      <!-- <HelloWorld/> -->
       <router-view></router-view>
     </v-content>
+    <Footer />
   </v-app>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,Navbar
+    HelloWorld,Navbar,Footer
   },
   data: () => ({
-    //
+    gradient: 'to top, #1867c0, #5cbbf6',
   }),
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+     <v-footer fixed light > 
+       <v-layout column align-center justify-center>
+           <v-row>
+               <h4 class="px-4 mt-2">Need Help? Watch Jayden's How To Videos:</h4>
+               <v-btn class="px-4">
+                    <v-icon small>videocam</v-icon>
+               </v-btn>
+           </v-row>
+           <v-row>
+                Copyright © {{ new Date().getFullYear() }} JMR Cloud Apps
+           </v-row>
+       </v-layout>
+       <!-- <v-jumbotron height=auto justify-center>
+       <v-container align-center justify-center>
+          <v-layout align-center justify-center>
+               <h4 class="px-4">Need Help? Watch Jayden's How To Videos:</h4>
+               <v-btn class="px-4">
+                    <v-icon small>videocam</v-icon>
+               </v-btn>
+          </v-layout>
+          <v-layout align-center justify-center>
+                Copyright © {{ new Date().getFullYear() }} JMR Cloud Apps
+          </v-layout>
+       </v-container>
+       </v-jumbotron> -->
+     </v-footer> 
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  components: { },
+  data() {
+    return {
+    }
+  },
+}
+</script>
